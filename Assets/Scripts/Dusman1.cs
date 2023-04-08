301d686aa5dbc3ca453a897731d81f26442d69f5
Burhan Keskin <101925448+BurhanKeskin@users.noreply.github.com>
2023-04-08 16:04:53 +0300

    burhan
    bolum 1'e dusmanlar eklendi

--- Assets/Scripts/Dusman1.cs
+++ Assets/Scripts/Dusman1.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Dusman : MonoBehaviour
+public class Dusman1 : MonoBehaviour
 {
     [SerializeField] Transform[] waypoints;
     [SerializeField] float movespeed;
@@ -29,6 +29,15 @@ public class Dusman : MonoBehaviour
             {
                 transform.position = Vector2.MoveTowards(transform.position, waypoints[1].position, movespeed * Time.deltaTime);
                 if (Vector2.Distance(transform.position, waypoints[1].position) < .2f)
+                {
+                    transform.localScale = new Vector2(-5.211222f, transform.localScale.y);
+                    waypointDest = 2;
+                }
+            }
+            else if (waypointDest == 2)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, waypoints[2].position, movespeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, waypoints[2].position) < .2f)
                 {
                     transform.localScale = new Vector2(-5.211222f, transform.localScale.y);
                     waypointDest = 0;
