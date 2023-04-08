1332fee10af9f90e0696271d5b70b8741f982983
Kaan Toraman <kaan03tor@gmail.com>
2023-04-08 23:44:14 +0300

    kaan
    ateş topu+animasyon

--- Assets/Scripts/CharacterControls.cs
+++ Assets/Scripts/CharacterControls.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
@@ -9,21 +10,39 @@ public class CharacterControls : MonoBehaviour
     [SerializeField] float walkspeed;
     private Rigidbody2D rb;
     Animator animator;
+    public bool left;
+    [SerializeField] GameObject bulletPrefab;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        left = false;
     }
    
     void Update()
     {
         Movement();
         Animate();
-
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Fire();
+            Vector2 shootDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            // Mermi prefabýnýn yönünü karakterin baktýðý yöne göre ayarla
+            if (shootDirection.x < 0)
+            {
+                bullet.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                bullet.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            bullet.GetComponent<Rigidbody2D>().velocity = shootDirection.normalized * 10f;
+            Destroy(bullet, 5f);
         }
+
     }
 
     void Movement()
@@ -45,18 +64,14 @@ public class CharacterControls : MonoBehaviour
         if (horizontalInput > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
+            left = true;
         }
         else if (horizontalInput < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+            left = false;
         }
     }
-    [SerializeField] Transform shootingPoint;
-    [SerializeField] GameObject prefab;
-    float fireSpeed = 10f;
-    private void Fire()
-    {
-        var fire = Instantiate(prefab, shootingPoint.position, shootingPoint.rotation);
-        fire.GetComponent<Rigidbody2D>().velocity = shootingPoint.up * fireSpeed;
-    }
+
+   
 }
