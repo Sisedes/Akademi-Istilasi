5787520c65583ac807af9fc5dcf88cb9221ad44c
Kaan Toraman <kaan03tor@gmail.com>
2023-04-09 21:10:24 +0300

    kaan
    kontroller geldi

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour
 {
+    [SerializeField] GameObject panel;
+    private void Start()
+    {
+        panel.SetActive(false);
+    }
+    public void Ayarlar()
+    {
+        panel.SetActive(true);
+    }
+    public void kappa()
+    {
+        panel.SetActive(false);
+    }
+
     public void oyunBaslat()
     {
         SceneManager.LoadScene(1);
